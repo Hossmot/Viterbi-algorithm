ca5dd6530cd57e600b1ab17d38a8e7d98d664c46
Hossmot <83452955+Hossmot@users.noreply.github.com>
2022-09-16 11:41:47 +0900

    Update VTB.py

--- VTB.py
+++ VTB.py
@@ -10,7 +10,6 @@ VIB_LS : Viterbi Last Step
 VTB_Finale : Literally.
 
 You can empty the particular list component to adjust the number of component.
-
 Made by Youngbo Sim
 """
 def InputPoint():
@@ -18,50 +17,42 @@ def InputPoint():
     global Chapter 
     Point = int(input('Input Point '))
     Chapter = int(input('Input Chapter '))
-
 def InputFirstPb():
     global FirstPb
     FirstPb = []
     for i in range(Point):
         print('FirstPb :'+str(i))
         FirstPb.append(float(input('Input FirstPb ')))
-
 def InputTsPb():
     global TsPb 
     TsPb = []
     for i in range(Point):
         print('TsPb : '+str(i))
         TsPb.append([float(input('Input TsPb ')) for j in range(Point)])
-
 def InputEmPb():
     global EmPb 
     EmPb = []
     for i in range(Point):
         print('EmPb : '+str(i))
         EmPb.append([float(input('Input EmPb ')) for j in range(Point)])
-
 def InputPb():
     InputFirstPb()
     InputTsPb()
     InputEmPb()
     global TotalPb
     TotalPb = FirstPb + TsPb + EmPb
-
 def InputEm():
     global Emission
     Emission = []
     for i in range(Chapter):
         Emission.append(float(input('Input Emission' + str(i) + ' ')))
-
 def VTB():
     global EachPointPb
     EachPointPb = []
     VTB_FS()
     VTB_LS()
-
 def VTB_FS():
     EachPointPb.append([FirstPb[i]*EmPb[i][int(Emission[0])] for i in range(Point)])
-
 def VTB_LS():
     for i in range(Chapter):
         if(i==0):
@@ -69,7 +60,6 @@ def VTB_LS():
         else:
             EachPointPb.append([ EmPb[j][int(Emission[i])] * max([EachPointPb[i-1][k] * TsPb[k][j]for k in range(Point)])for j in range(Point)])    
         print(i)      
-
 def VTB_Finale():
     global OutcomePoint
     global OutcomePb
